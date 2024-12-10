1be8bb7b5afc18365bb3be38559d53228a5e7ae3
pedrocarvalho3 <pdrpvp0310@gmail.com>
2024-12-10 15:02:20 -0300

    feature: vitest configured for tests

---
+++ src/use-cases/regiter.spec.ts
@@ -0,0 +1,24 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'hRb3t@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
