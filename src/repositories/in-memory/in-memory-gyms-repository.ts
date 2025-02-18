3cafb7a5ccd31b2362347051a4008c68de53c0fb
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-02-18 13:11:00 -0300

    feat: fetch-nearby-gym use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,15 +1,36 @@
-import { randomUUID } from 'node:crypto'
-import type { GymsRepository } from '../gyms-repository'
+import {
+  FindManyNearbyParams,
+  GymsRepository,
+} from '@/repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { Gym, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
 
   async findById(id: string) {
     const gym = this.items.find((item) => item.id === id)
+
     if (!gym) {
       return null
     }
+
     return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
   }
 
   async searchMany(query: string, page: number) {
@@ -28,7 +49,9 @@ export class InMemoryGymsRepository implements GymsRepository {
       longitude: new Prisma.Decimal(data.longitude.toString()),
       created_at: new Date(),
     }
+
     this.items.push(gym)
+
     return gym
   }
 }
