f0d7b02a76804a2d4700f518a30620acb4c0882b
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-03-09 13:25:06 -0300

    feat: JWT authentication principles

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
