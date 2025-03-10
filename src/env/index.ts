11ad725f50b50aa8646999b12f99891d5e8fa9f1
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-03-10 11:59:39 -0300

    feat: implementing JWT in Fastify

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   // DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   // DATABASE_URL: z.string(),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 })
 
